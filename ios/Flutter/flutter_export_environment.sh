2ec4a854ec232f275265ff43d2e6413b550e02b7
Bennet303 <bennet.hoelscher@outlook.com>
2020-01-25 21:31:38 +0100

    initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=P:\Frameworks\flutter_windows_v1.9.1+hotfix.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\OneDrive\Privat\RB5-26\Dokumente\Programmieren\Projekte\Flutter\fahrtenbuch"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=P:\Frameworks\flutter_windows_v1.9.1+hotfix.4-stable\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
