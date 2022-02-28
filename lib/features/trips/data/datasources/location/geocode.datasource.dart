5db708c381394b4634841992a9d20554ff08d0a3
Bennet303 <bennet.hoelscher@outlook.com>
2022-02-28 22:26:46 +0100

    fix dependencies

--- lib/features/trips/data/datasources/location/geocode.datasource.dart
+++ lib/features/trips/data/datasources/location/geocode.datasource.dart
@@ -1,17 +1,17 @@
-import 'package:fahrtenbuch/features/trips/data/datasources/location/location.datasource.dart';
-import 'package:fahrtenbuch/features/trips/data/models/coordinates.model.dart';
-import 'package:geocode/geocode.dart';
+// import 'package:fahrtenbuch/features/trips/data/datasources/location/location.datasource.dart';
+// import 'package:fahrtenbuch/features/trips/data/models/coordinates.model.dart';
+// import 'package:geocode/geocode.dart';
 
-class GeocodeDatasource extends LocationDatasource {
-  @override
-  Future<String> getLocationName(CoordinatesModel coordinates) async {
-    GeoCode geoCode = GeoCode();
-    Address address = await geoCode.reverseGeocoding(
-        latitude: coordinates.latitude, longitude: coordinates.longitude);
-    final cityName = address.city;
-    if (cityName == null) {
-      throw Exception('No locality found');
-    }
-    return cityName;
-  }
-}
+// class GeocodeDatasource extends LocationDatasource {
+//   @override
+//   Future<String> getLocationName(CoordinatesModel coordinates) async {
+//     // GeoCode geoCode = GeoCode();
+//     // Address address = await geoCode.reverseGeocoding(
+//     //     latitude: coordinates.latitude, longitude: coordinates.longitude);
+//     // final cityName = address.city;
+//     // if (cityName == null) {
+//     //   throw Exception('No locality found');
+//     // }
+//     // return cityName;
+//   }
+// }
